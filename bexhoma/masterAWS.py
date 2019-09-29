8245250a60ccab7f2f9954b3f910093dc0deca83
Patrick Erdelt <patrick.erdelt@beuth-hochschule.de>
2019-09-29 21:35:58 +0200

    Masterscript: Wait 30s after unpark

--- bexhoma/masterAWS.py
+++ bexhoma/masterAWS.py
@@ -350,7 +350,7 @@ class testdesign():
         cmd['rename_docker_container'] = "docker rename "+connection+" benchmark"
         stdin, stdout, stderr = self.executeSSH(cmd['rename_docker_container'])
         self.restartDocker()
-        self.wait(10)
+        self.wait(30)
     def parkExperiment(self):
         print("parkExperiment")
         self.stopDocker()
