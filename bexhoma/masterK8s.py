70f4d742b02856d95af04ba51489a0fa703137f8
Patrick Erdelt <patrick.erdelt@beuth-hochschule.de>
2020-08-20 19:27:08 +0200

    K8s: bexhoma-service fixed

--- bexhoma/masterK8s.py
+++ bexhoma/masterK8s.py
@@ -398,7 +398,7 @@ class testdesign():
         #}
         #portstring = " ".join([str(k)+":"+str(v) for k,v in ports.items()])
         if len(self.deployments) > 0:
-            forward = ['kubectl', 'port-forward', 'service/service-dbmsbenchmarker']#, '9091', '9300']#, '9400']
+            forward = ['kubectl', 'port-forward', 'service/bexhoma-service']#, '9091', '9300']#, '9400']
             forward.extend(ports)
             #forward = ['kubectl', 'port-forward', 'service/service-dbmsbenchmarker', '9091', '9300']#, '9400']
             #forward = ['kubectl', 'port-forward', 'service/service-dbmsbenchmarker', portstring]
