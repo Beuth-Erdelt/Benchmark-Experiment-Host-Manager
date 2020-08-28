5c87111d3ab638091cf1e9f0ed13095ad26a72d3
Patrick Erdelt <patrick.erdelt@beuth-hochschule.de>
2020-08-28 09:05:25 +0200

    Legal notice in source code

--- bexhoma/experiments.py
+++ bexhoma/experiments.py
@@ -1,3 +1,20 @@
+"""
+    Class to run workflow given as Python Dict for bexhoma
+    Copyright (C) 2020  Patrick Erdelt
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 from os import makedirs, path
 import ast
 from bexhoma import masterAWS, masterK8s
--- experiment-example-AWS.py
+++ experiment-example-AWS.py
@@ -1,11 +1,19 @@
 """
-:Date: 2018-09-10
-:Version: 0.3.6
-:Authors: Patrick Erdelt
+    Small demo for running experiments in an AWS cluster. This deals with some TPC-H tests for bexhoma.
+    Copyright (C) 2020  Patrick Erdelt
 
-Small demo for running experiments in an k8s cluster
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
-This deals with the TPC-H tests.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 from bexhoma import *
 import logging
