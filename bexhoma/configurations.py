2816b19641f1c050f521f32869f883f6ec6049f8
Patrick Erdelt <patrick.erdelt@beuth-hochschule.de>
2021-03-15 13:14:34 +0100

    Masterscript: K8s details of experiment structure - monitoring test

--- bexhoma/configurations.py
+++ bexhoma/configurations.py
@@ -80,7 +80,7 @@ class default():
         self.client = 1
         self.dialect = dialect
         self.num_worker = worker
-        self.monitoring_active = False
+        self.monitoring_active = experiment.monitoring_active
         # per configuration: sut+service
         # per configuration: monitoring+service
         # per configuration: list of benchmarker
