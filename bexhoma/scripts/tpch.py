2699a710e3a347478bcacf805195c171715cff82
Patrick Erdelt <patrick.erdelt@beuth-hochschule.de>
2021-08-23 13:48:08 +0200

    TPC-H: Éxample script

--- bexhoma/scripts/tpch.py
+++ bexhoma/scripts/tpch.py
@@ -32,19 +32,26 @@ def do_benchmark():
 	# argparse
 	parser = argparse.ArgumentParser(description=description)
 	parser.add_argument('mode', help='profile the import or run the TPC-H queries', choices=['profiling', 'run'])
+	parser.add_argument('-cx', '--context', help='context of Kubernetes (for a multi cluster environment), default is current context', default=None)
 	parser.add_argument('-e', '--experiment', help='sets experiment code for continuing started experiment', default=None)
 	parser.add_argument('-d', '--detached', help='puts most of the experiment workflow inside the cluster', action='store_true')
 	parser.add_argument('-m', '--monitoring', help='activates monitoring', action='store_true')
+	parser.add_argument('-ms', '--max-sut', help='maximum number of parallel DBMS configurations, default is no limit', default=None)
+	parser.add_argument('-dt', '--datatransfer', help='activates datatransfer', action='store_true', default=False)
 	parser.add_argument('-md', '--monitoring-delay', help='time to wait [s] before execution of the runs of a query', default=10)
 	parser.add_argument('-nr', '--num-run', help='number of runs per query', default=1)
 	parser.add_argument('-nc', '--num-config', help='number of runs per configuration', default=1)
+	parser.add_argument('-ne', '--num-query-executors', help='comma separated list of number of parallel clients', default="1")
 	parser.add_argument('-sf', '--scaling-factor', help='scaling factor (SF)', default=1)
 	parser.add_argument('-t', '--timeout', help='timeout for a run of a query', default=180)
-	parser.add_argument('-rc', '--request-cpu', help='request cpus', default='2')
-	parser.add_argument('-rr', '--request-ram', help='request ram', default='64Gi')
-	parser.add_argument('-rg', '--request-gpu', help='request number of gpus', default=1)
+	parser.add_argument('-rr', '--request-ram', help='request ram', default='16Gi')
+	parser.add_argument('-rc', '--request-cpu', help='request cpus', default='4')
 	parser.add_argument('-rct', '--request-cpu-type', help='request node having node label cpu=', default='')
-	parser.add_argument('-rgt', '--request-gpu-type', help='request node having node label gpu=', default='p100')
+	parser.add_argument('-rg', '--request-gpu', help='request number of gpus', default=1)
+	parser.add_argument('-rgt', '--request-gpu-type', help='request node having node label gpu=', default='a100')
+	parser.add_argument('-rst', '--request-storage-type', help='request persistent storage of certain type', default=None, choices=[None, '', 'local-hdd', 'shared'])
+	parser.add_argument('-rss', '--request-storage-size', help='request persistent storage of certain size', default='10Gi')
+	parser.add_argument('-rnn', '--request-node-name', help='request a specific node', default=None)
 	args = parser.parse_args()
 	# set parameter
 	monitoring = args.monitoring
@@ -58,16 +65,20 @@ def do_benchmark():
 	cpu_type = str(args.request_cpu_type)
 	gpu_type = str(args.request_gpu_type)
 	gpus = str(args.request_gpu)
+	request_storage_type = args.request_storage_type
+	request_storage_size = args.request_storage_size
+	request_node_name = args.request_node_name
+	datatransfer = args.datatransfer
 	code = args.experiment
 	# set cluster
-	cluster = clusters.kubernetes()
+	cluster = clusters.kubernetes(context=args.context)
+	cluster_name = cluster.contextdata['clustername']
+	if args.max_sut is not None:
+		cluster.max_sut = int(args.max_sut)
 	# set experiment
-	#experiment = experiments.tpch(cluster=cluster, SF=SF, timeout=timeout*numRun, numExperiments=numExperiments, detached=args.detached, code=cluster.code)
 	if code is None:
 		code = cluster.code
-	experiment = experiments.tpch(cluster=cluster, SF=SF, timeout=timeout, numExperiments=1, detached=True, code=code)
-	# remove running dbms
-	#experiment.clean()
+	experiment = experiments.tpch(cluster=cluster, SF=SF, timeout=timeout, detached=True, code=code, numExperiments=numExperiments)
 	if mode == 'run':
 		# we want all TPC-H queries
 		experiment.set_queries_full()
@@ -84,12 +95,11 @@ def do_benchmark():
 		)
 	if monitoring:
 		# we want to monitor resource consumption
-		experiment.set_querymanagement_monitoring(numRun=numRun, delay=10)
+		experiment.set_querymanagement_monitoring(numRun=numRun, delay=10, datatransfer=datatransfer)
 	else:
 		# we want to just run the queries
-		experiment.set_querymanagement_quicktest(numRun=numRun)
+		experiment.set_querymanagement_quicktest(numRun=numRun, datatransfer=datatransfer)
 	# set resources for dbms
-	#experiment.connectionmanagement['timeout'] = 180
 	experiment.set_resources(
 		requests = {
 			'cpu': cpu,
@@ -102,8 +112,22 @@ def do_benchmark():
 		},
 		nodeSelector = {
 			'cpu': cpu_type,
-			'gpu': ''
+			'gpu': '',
 		})
+	if request_node_name is not None:
+		experiment.set_resources(
+			nodeSelector = {
+				'cpu': cpu_type,
+				'gpu': '',
+				'kubernetes.io/hostname': request_node_name
+			})		
+	# persistent storage
+	print(request_storage_type)
+	experiment.set_storage(
+		storageClassName = request_storage_type,
+		storageSize = request_storage_size,#'100Gi',
+		keep = False
+		)
 	cluster.start_dashboard()
 	# add configs
 	config = configurations.default(experiment=experiment, docker='MonetDB', alias='DBMS A', numExperiments=1, clients=[1])
@@ -114,63 +138,12 @@ def do_benchmark():
 	config = configurations.default(experiment=experiment, docker='MySQL', alias='DBMS E', numExperiments=1, clients=[1])
 	config = configurations.default(experiment=experiment, docker='MariaDBCS', alias='DBMS F', numExperiments=1, clients=[1])
 	config = configurations.default(experiment=experiment, docker='Exasol', alias='DBMS G', numExperiments=1, clients=[1])
-	config.set_resources(
-		requests = {
-			'cpu': cpu,
-			'memory': memory,
-			'gpu': 0
-		},
-		limits = {
-			'cpu': 0,
-			'memory': 0
-		},
-		nodeSelector = {
-			'cpu': 'epyc-7742',
-			'gpu': ''
-		})
 	config = configurations.default(experiment=experiment, docker='DB2', alias='DBMS H', numExperiments=1, clients=[1])
-	config.set_resources(
-		requests = {
-			'cpu': cpu,
-			'memory': memory,
-			'gpu': 0
-		},
-		limits = {
-			'cpu': 0,
-			'memory': 0
-		},
-		nodeSelector = {
-			'cpu': 'epyc-7742',
-			'gpu': ''
-		})
 	config = configurations.default(experiment=experiment, docker='SAPHANA', alias='DBMS I', numExperiments=1, clients=[1])
 	config = configurations.default(experiment=experiment, docker='Clickhouse', alias='DBMS J', numExperiments=1, clients=[1])
 	config = configurations.default(experiment=experiment, docker='SQLServer', alias='DBMS K', numExperiments=1, clients=[1])
 	config = configurations.default(experiment=experiment, docker='OmniSci', alias='DBMS L', numExperiments=1, clients=[1])
-	shard_count = int(gpus)
-	fragment_size = int(2*64000000/shard_count)
-	config.set_ddl_parameters(fragment_size=str(fragment_size), shard_count=str(shard_count))
-	config.set_experiment(script='SF'+SF+'-template')
-	config.set_resources(
-		requests = {
-			'cpu': cpu,
-			'memory': memory,
-			'gpu': gpus
-		},
-		limits = {
-			'cpu': 0,
-			'memory': 0
-		},
-		nodeSelector = {
-			'cpu': '',
-			'gpu': gpu_type
-		})
-	#config = configurations.default(experiment=experiment, docker='Citus', configuration='Citus-2w', alias='DBMS M', numExperiments=1, clients=[1], dialect='OmniSci', worker=2)
-	#config = configurations.default(experiment=experiment, docker='Citus', configuration='Citus-4w', alias='DBMS N', numExperiments=1, clients=[1], dialect='OmniSci', worker=4)
 	experiment.start_sut()
-	#experiment.wait(180)
-	#experiment.start_monitoring()
-	#experiment.start_loading()
 	list_clients = [1]
 	experiment.add_benchmark_list(list_clients)
 	# add list to all config
@@ -179,8 +152,6 @@ def do_benchmark():
 	# yes: work on list
 	# no: try to load
 	experiment.work_benchmark_list()
-	#experiment.benchmark_list(list_clients)
-	##################
 	experiment.evaluate_results()
 	experiment.stop_benchmarker()
 	experiment.stop_sut()
