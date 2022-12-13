9cadda99f64d46f442c9130c9ab7c9dc6feda401
Patrick Erdelt <patrick.erdelt@beuth-hochschule.de>
2022-12-13 08:32:11 +0100

    Masterscript: Name of worker storage contains experiment code

--- bexhoma/configurations.py
+++ bexhoma/configurations.py
@@ -1253,7 +1253,7 @@ scrape_configs:
                 # remove the storage
                 name_pvc = self.generate_component_name(app=app, component='storage', experiment=self.storage_label, configuration=configuration)
                 self.experiment.cluster.delete_pvc(name_pvc)
-                name_pvc = self.generate_component_name(app=app, component='worker', experiment=self.storage_label, configuration=configuration)
+                name_pvc = self.generate_component_name(app=app, component='worker', experiment=experiment, configuration=configuration)
                 self.experiment.cluster.delete_pvc(name_pvc)
         deployments = self.experiment.cluster.get_deployments(app=app, component=component, experiment=experiment, configuration=configuration)
         for deployment in deployments:
