d43037237b46b40551f8c1fb16fc4545f4365061
perdelt <perdelt@bht-berlin.de>
2023-01-06 18:32:58 +0100

    Masterscript: Copy connection file for connection specified by name

--- bexhoma/configurations.py
+++ bexhoma/configurations.py
@@ -1748,7 +1748,7 @@ scrape_configs:
             f.write(str(self.benchmark.connections))
         filename = self.benchmark.path+'/'+c['name']+'.config'
         with open(filename, 'w') as f:
-            f.write(str(c))
+            f.write(str([c]))
         # write appended query config
         if len(self.experiment.workload) > 0:
             for k,v in self.experiment.workload.items():
