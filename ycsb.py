7c7abb9fc6721f80b2a045ea6d0f571f28ef5155
perdelt <perdelt@bht-berlin.de>
2023-11-29 13:59:33 +0100

    YCSB: Default options

--- ycsb.py
+++ ycsb.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     parser.add_argument('-nc', '--num-config', help='number of runs per configuration', default=1)
     parser.add_argument('-ne', '--num-query-executors', help='comma separated list of number of parallel clients', default="1")
     parser.add_argument('-nl', '--num-loading', help='number of parallel loaders per configuration', default=1)
-    parser.add_argument('-nlp', '--num-loading-pods', help='total number of loaders per configuration', default=[1,8])
+    parser.add_argument('-nlp', '--num-loading-pods', help='total number of loaders per configuration', default="1,8")
     parser.add_argument('-sf', '--scaling-factor', help='scaling factor (SF) = number of rows in millions', default=1)
     parser.add_argument('-sfo', '--scaling-factor-operations', help='scaling factor (SF) = number of operations in millions (=SF if not set)', default=None)
     parser.add_argument('-su', '--scaling-users', help='scaling factor = number of total threads', default=64)
@@ -77,7 +77,7 @@ if __name__ == '__main__':
     mode = str(args.mode)
     SF = str(args.scaling_factor)
     SFO = str(args.scaling_factor_operations)
-    if SFO is None:
+    if SFO == 'None':
         SFO = SF
     SU = int(args.scaling_users)
     target_base = int(args.target_base)
