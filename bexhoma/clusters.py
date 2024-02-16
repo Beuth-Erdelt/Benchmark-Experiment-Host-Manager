7e0bc95c3c3645f105bf9e0e55f15a5e880d9d5b
Patrick Erdelt <perdelt@bht-berlin.de>
2024-02-16 15:28:44 +0100

    Bexhoma: test_if_monitoring_healthy() with cURL

--- bexhoma/clusters.py
+++ bexhoma/clusters.py
@@ -1335,7 +1335,7 @@ class testbed():
                 self.logger.debug('Command {}'.format(command))
                 #fullcommand = 'kubectl exec '+self.pod_sut+' --container=dbms -- bash -c "'+command+'"'
                 #cores = os.popen(fullcommand).read()
-                stdin, stdout, stderr = self.execute_command_in_pod_sut(pod=pod_dashboard, command=command)
+                stdin, stdout, stderr = self.execute_command_in_pod(pod=pod_dashboard, command=command)
                 print("Return", stdout, stderr)
                 status = stdout#os.popen(fullcommand).read()
                 if len(status)>0:
