648ee0861a0c0d81f503db253a72abad13e64ee1
perdelt <perdelt@bht-berlin.de>
2024-02-19 20:26:03 +0100

    Bexhoma: show localresults

--- cluster.py
+++ cluster.py
@@ -27,6 +27,7 @@ import pandas as pd
 from tabulate import tabulate
 from datetime import datetime
 import multiprocessing as mp
+from prettytable import PrettyTable, ALL
 
 urllib3.disable_warnings()
 logging.basicConfig(level=logging.ERROR)
@@ -37,7 +38,7 @@ if __name__ == '__main__':
     """
     # argparse
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('mode', help='manage experiments: stop, get status, connect to dbms or connect to dashboard', choices=['stop','status','dashboard','localdashboard','jupyter','master'])
+    parser.add_argument('mode', help='manage experiments: stop, get status, connect to dbms or connect to dashboard', choices=['stop','status','dashboard','localdashboard','localresults','jupyter','master'])
     parser.add_argument('-db', '--debug', help='dump debug informations', action='store_true')
     parser.add_argument('-e', '--experiment', help='code of experiment', default=None)
     parser.add_argument('-c', '--connection', help='name of DBMS', default=None)
@@ -84,6 +85,26 @@ if __name__ == '__main__':
         sys.argv.remove('localdashboard')
         from dbmsbenchmarker.scripts import dashboardcli
         dashboardcli.startup()
+    elif args.mode == 'localresults':
+        cluster = clusters.kubernetes(clusterconfig, context=args.context)
+        # path of folder containing experiment results
+        resultfolder = cluster.resultfolder
+        # create evaluation object for result folder
+        evaluate = inspector.inspector(resultfolder)
+        # dataframe of experiments
+        df = evaluate.get_experiments_preview().sort_values('time')
+        df = df.reset_index()
+        df['info'] = df['info'].str.replace('. ', '.\n')
+        # Create a PrettyTable object
+        pt = PrettyTable()
+        pt.field_names = df.columns
+        pt.align['info'] = 'r'  # 'r' for right alignment
+        pt.hrules=ALL
+        # Add rows to the PrettyTable
+        for _, row in df.iterrows():
+            pt.add_row(row)
+        # Display the PrettyTable
+        print(pt)
     elif args.mode == 'jupyter':
         import subprocess
         cmd = ["jupyter","notebook","--notebook-dir","images/evaluator_dbmsbenchmarker/notebooks","--NotebookApp.ip","0.0.0.0","--no-browser","--NotebookApp.allow_origin","*"]
