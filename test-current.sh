bf77b7e9b22790762de2138839af9715a0a4f2d0
Patrick Erdelt <patrick.erdelt@bht-berlin.de>
2025-04-25 16:26:59 +0200

    Tests: Data Profiling with TPC-DS to inspect nodes

--- test-current.sh
+++ test-current.sh
@@ -60,6 +60,8 @@ ready_nodes=($(
   ) | grep -v master | awk '$2 == "Ready" {print $1}' | sort -u
 ))
 
+ready_nodes=("cl-worker17" "cl-worker21" "cl-worker34")
+
 for node in "${ready_nodes[@]}"; do
   echo "Ready node: $node"
   nohup python tpcds.py -dt -tr \
@@ -77,25 +79,48 @@ done
 
 
 
-
+node="cl-worker15"
 
 #### TCP-H Monitoring (Example-TPC-H.md)
-nohup python tpcds.py -ms 1 -dt -tr \
+nohup python tpcds.py -dt -tr \
   -dbms MonetDB \
   -nlp 8 \
   -nlt 8 \
-  -sf 10 \
+  -ne 1,1 \
+  -sf 30 \
   -ii -ic -is \
   -m -mc \
-  -rnn $BEXHOMA_NODE_SUT -rnl $BEXHOMA_NODE_LOAD -rnb $BEXHOMA_NODE_BENCHMARK \
-  profiling </dev/null &>$LOG_DIR/doc_tpcds_testcase_profiling_$node.log &
+  -rnn $node \
+  profiling </dev/null &>$LOG_DIR/doc_tpcds_testcase_profiling_30_$node.log &
 
 #### Wait so that next experiment receives a different code
 #sleep 600
 wait_process "tpcds"
 
 
+node="cl-worker36"
 
+
+BEXHOMA_YCSB_SF_DATA=16
+BEXHOMA_YCSB_SF_OPS=16
+
+python ycsb.py -tr \
+  -sf $BEXHOMA_YCSB_SF_DATA \
+  -sfo $BEXHOMA_YCSB_SF_OPS \
+  --workload c \
+  -dbms PostgreSQL \
+  -rnn $node \
+  -tb 16384 \
+  -nlp 8 \
+  -nlt 64 \
+  -nlf 12 \
+  -nbp 1 \
+  -nbt 128 \
+  -nbf 11 \
+  -ne 1,1 \
+  -nc 1 \
+  -m -mc \
+  run </dev/null &>$LOG_DIR/doc_ycsb_testcase_profiling_$node.log &
 
 
 
