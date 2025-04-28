bc39ffae749beca745fe427b91fa1fa93b932710
Patrick Erdelt <patrick.erdelt@bht-berlin.de>
2025-04-28 11:43:51 +0200

    Tests: More benchmarks about node performance

--- test-current.sh
+++ test-current.sh
@@ -46,13 +46,21 @@ wait_process "ycsb"
 kill $(ps aux | grep MonetDB | head -n 1 | awk '{print $2}')
 
 
+###########################################
+############### Find Nodes ################
+###########################################
 
+
+# list all cpu or gpu nodes, that are not master
 (
   kubectl get nodes -l node-role.kubernetes.io/cpu --no-headers
   kubectl get nodes -l node-role.kubernetes.io/gpu --no-headers
 ) | grep -v master | awk '$2 == "Ready" {print $1}' | sort -u
 
 
+# list all cpu or gpu nodes, that are not master
+# pack into list
+# ready_nodes=("cl-worker17" "cl-worker21" "cl-worker34")
 ready_nodes=($(
   (
     kubectl get nodes -l node-role.kubernetes.io/cpu --no-headers
@@ -60,8 +68,14 @@ ready_nodes=($(
   ) | grep -v master | awk '$2 == "Ready" {print $1}' | sort -u
 ))
 
-ready_nodes=("cl-worker17" "cl-worker21" "cl-worker34")
 
+
+###########################################
+############ TPC-DS Profiling #############
+###########################################
+
+
+# MonetDB profiles TPC-DS @SF=30
 for node in "${ready_nodes[@]}"; do
   echo "Ready node: $node"
   nohup python tpcds.py -dt -tr \
@@ -77,58 +91,18 @@ for node in "${ready_nodes[@]}"; do
   sleep 3600
 done
 
+
+###########################################
+############### YCSB Read #################
+###########################################
+
+# PostgreSQL runs YCSB workload C (100% read) at 16 warehouses
 BEXHOMA_YCSB_SF_DATA=16
 BEXHOMA_YCSB_SF_OPS=128
 
 
-for node in "${ready_nodes[@]}"; do
-  python ycsb.py -tr \
-  -sf $BEXHOMA_YCSB_SF_DATA \
-  -sfo $BEXHOMA_YCSB_SF_OPS \
-  --workload c \
-  -dbms PostgreSQL \
-  -rnn $node \
-  -tb 16384 \
-  -nlp 8 \
-  -nlt 64 \
-  -nlf 12 \
-  -nbp 1 \
-  -nbt 128 \
-  -nbf 11 \
-  -ne 1,1 \
-  -nc 1 \
-  -m -mc \
-  run </dev/null &>$LOG_DIR/doc_ycsb_testcase_profiling_$node.log &
-  sleep 3600
-done
-
-
-
-
-node="cl-worker15"
-
-#### TCP-H Monitoring (Example-TPC-H.md)
-nohup python tpcds.py -dt -tr \
-  -dbms MonetDB \
-  -nlp 8 \
-  -nlt 8 \
-  -ne 1,1 \
-  -sf 30 \
-  -ii -ic -is \
-  -m -mc \
-  -rnn $node \
-  profiling </dev/null &>$LOG_DIR/doc_tpcds_testcase_profiling_30_$node.log &
-
-#### Wait so that next experiment receives a different code
-#sleep 600
-wait_process "tpcds"
-
-
+# Single test
 node="cl-worker36"
-
-
-BEXHOMA_YCSB_SF_DATA=16
-BEXHOMA_YCSB_SF_OPS=128
 
 python ycsb.py -tr \
   -sf $BEXHOMA_YCSB_SF_DATA \
@@ -149,12 +123,69 @@ python ycsb.py -tr \
   run </dev/null &>$LOG_DIR/doc_ycsb_testcase_profiling_$node.log &
 
 
+# loop over all nodes
+for node in "${ready_nodes[@]}"; do
+  nohup python ycsb.py -tr \
+  -sf $BEXHOMA_YCSB_SF_DATA \
+  -sfo $BEXHOMA_YCSB_SF_OPS \
+  --workload c \
+  -dbms PostgreSQL \
+  -rnn $node \
+  -tb 16384 \
+  -nlp 8 \
+  -nlt 64 \
+  -nlf 12 \
+  -nbp 1 \
+  -nbt 128 \
+  -nbf 11 \
+  -ne 1,1 \
+  -nc 1 \
+  -m -mc \
+  run </dev/null &>$LOG_DIR/doc_ycsb_testcase_profiling_$node.log &
+  sleep 3600
+done
 
 
 
 
+###########################################
+############## HammerDB TPC-C #############
+###########################################
 
 
+# Single test
+nohup python hammerdb.py -tr \
+  -sf 16 \
+  -sd 20 \
+  -xlat \
+  -dbms PostgreSQL \
+  -nlt 16 \
+  -nbp 1 \
+  -nbt 16 \
+  -m -mc \
+  -ne 1,1 \
+  -nc 1 \
+  -rnn $node \
+  run </dev/null &>$LOG_DIR/doc_hammerdb_testcase_profiling_$node.log &
+
+
+# loop over all nodes
+for node in "${ready_nodes[@]}"; do
+  nohup python hammerdb.py -tr \
+  -sf 16 \
+  -sd 20 \
+  -xlat \
+  -dbms PostgreSQL \
+  -nlt 16 \
+  -nbp 1 \
+  -nbt 16 \
+  -m -mc \
+  -ne 1,1 \
+  -nc 1 \
+  -rnn $node \
+  run </dev/null &>$LOG_DIR/doc_hammerdb_testcase_profiling_$node.log &
+  sleep 3600
+done
 
 
 
