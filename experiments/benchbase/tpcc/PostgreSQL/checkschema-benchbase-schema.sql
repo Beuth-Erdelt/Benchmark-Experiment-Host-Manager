f61706e13650aa4da02a979bfc809a717dd9bee1
Patrick Erdelt <patrick.erdelt@bht-berlin.de>
2025-07-14 17:08:08 +0200

    PostgreSQL: fetch metric from several databases possible?

--- experiments/benchbase/tpcc/PostgreSQL/checkschema-benchbase-schema.sql
+++ experiments/benchbase/tpcc/PostgreSQL/checkschema-benchbase-schema.sql
@@ -12,3 +12,25 @@ VACUUM ANALYZE {BEXHOMA_SCHEMA}.item;
 SELECT COUNT(*) AS "count warehouses" FROM {BEXHOMA_SCHEMA}.warehouse;
 
 
+SELECT name, setting FROM pg_settings WHERE source != 'default';
+
+SELECT relname, last_autovacuum, n_dead_tup
+FROM pg_stat_user_tables
+ORDER BY n_dead_tup DESC
+LIMIT 10;
+
+SELECT relname, last_analyze, n_live_tup
+FROM pg_stat_user_tables
+ORDER BY last_analyze NULLS FIRST;
+
+
+SELECT relname, n_dead_tup, n_live_tup
+FROM pg_stat_user_tables
+ORDER BY n_dead_tup DESC;
+
+SELECT current_database(), current_user, current_schema();
+
+SHOW search_path;
+
+
+
