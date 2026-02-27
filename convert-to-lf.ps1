2ff1630b4c02949406bdc5cd619789059a6dfb07
perdelt <perdelt@bht-berlin.de>
2026-02-27 18:43:24 +0100

    Bexhoma: images folder linux and utf-8

--- convert-to-lf.ps1
+++ convert-to-lf.ps1
@@ -1,10 +1,11 @@
 $paths = @(
     "docs",
-    "logs_tests"
+    "logs_tests",
+    "images"
 )
 
 foreach ($path in $paths) {
-    Get-ChildItem -Path $path -File -Recurse -Include *.md, *.txt | ForEach-Object {
+    Get-ChildItem -Path $path -File -Recurse -Include *.md, *.txt, Dockerfile, *sh | ForEach-Object {
 
         # --- Dateiinhalt in UTF-8 einlesen ---
         $utf8 = New-Object System.Text.UTF8Encoding($true)  # BOM optional erlauben
