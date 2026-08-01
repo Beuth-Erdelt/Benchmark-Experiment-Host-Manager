b560aca37ebb1c75ebc229b616592cd27a3f2be7
perdelt <perdelt@bht-berlin.de>
2026-08-01 09:32:03 +0200

    DBMSBenchmarker: v0.14.27

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -182,7 +182,7 @@ function Build-AndPush-Hardware {
 
 #### Main ####
 
-$dbmsbenchmarker = "v0.14.26"
+$dbmsbenchmarker = "v0.14.27"
 $version = (pip show bexhoma | Select-String "^Version:" | ForEach-Object { ($_ -split '\s+')[1] })
 Write-Output $version
 
